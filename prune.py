393bc14b0c6efaee18dbe43dc0ebc6b39812f894
Raluca Tudor <tudorr30@gmail.com>
2024-01-18 15:10:42 +0200

    Refactor pruning method to reduce branching

--- prune.py
+++ prune.py
@@ -3,7 +3,9 @@ import common
 import commonApi
 
 
-def pruning(dependency_tree, root, dependencies, token, tokens_remain, sentence):
+def pruning(
+    dependency_tree, root, dependencies, tokens, remaining_tokens_indices, sentence
+):
     """
     Applies the novel core semantics-preserving pruning strategy.
     Args:
@@ -11,102 +13,102 @@ def pruning(dependency_tree, root, dependencies, token, tokens_remain, sentence)
         root: The root node of the dependency tree.
         dependencies: List of dependency relationships.
         token: List of tokens.
-        tokens_remain: List of indices indicating the tokens to be retained.
+        remaining_tokens_indices: List of indices indicating the tokens to be retained.
         sentence: Current source sentence.
     """
 
-    if len(dependency_tree.children(root)) > 0:
-        nodes = dependency_tree.children(root)
-        # Nodes with the number of child nodes equal to 0 are arranged from left to right in the order in the relationship tree.
-        nodes1 = [x for x in nodes if len(dependency_tree.children(x.identifier)) == 0]
-        # Nodes with the number of child nodes greater than 0 are arranged from large to small according to the number of child nodes.
-        nodes2 = [x for x in nodes if len(dependency_tree.children(x.identifier)) != 0]
-        nodes2.sort(key=lambda x: len(dependency_tree.children(x.identifier)))
-        nodes2.reverse()
-
-        nodes = nodes2 + nodes1
-
-        # Start recursively traversing the dependency tree.
-        for node in nodes:
-            index = node.identifier
-            tag = node.data.type
-            if (
-                # Relationship 1 refers to either of the following:
-                # dependent (dep)
-                # auxiliary verbs or copula (aux, auxpass, cop),
-                # clausal complement and open clausal complement (ccomp, xcomp),
-                # expletive (expl) - e.g. "there are", "here is" and "it is", "indeed", “in fact”
-                # determiner (det)
-                # multi-word expression (mwe)
-                # marker (mark)
-                # particle (prt)
-                # goes with (goeswith)
-                # case marker (case)
-                common.relationship.get(tag, -10) == 1
-                # No relationship.
-                or common.relationship.get(tag, -10) == -10
-                # Relationship 3 refers to either of the following:
-                # Object-related dependencies (obj, dobj, iobj)
-                # Subject-related dependencies (nsubj, csubj, xsubj)
-                or common.relationship.get(tag, -10) == 3
-            ):
-                if len(dependency_tree.children(index)) > 0:
-                    sentence, tokens_remain = pruning(
-                        dependency_tree,
-                        index,
-                        dependencies,
-                        token,
-                        tokens_remain,
-                        sentence,
-                    )
-            # punctuation (punct)
-            # parataxis
-            elif common.relationship.get(tag, -10) == 4:
-                return sentence, tokens_remain
-            # adjectival clause modifier (acl)
-            # adverbial modifier (advmod)
-            # compound (compound)
-            elif common.relationship.get(tag, -10) == 0:
-                if len(dependency_tree.children(index)) > 0:
-                    sentence, tokens_remain = pruning(
-                        dependency_tree,
-                        index,
-                        dependencies,
-                        token,
-                        tokens_remain,
-                        sentence,
-                    )
-                    # After pruning the subtree
-                    if len(dependency_tree.children(index)) >= 0:
-                        cut_subtree_from_dependency_tree(
-                            dependency_tree,
-                            index,
-                            tokens_remain,
-                            dependencies,
-                            token,
-                        )
-                # If there is no need to retain
-                elif not commonApi.should_retain(index):
-                    cut_subtree_from_dependency_tree(
-                        dependency_tree,
-                        index,
-                        tokens_remain,
-                        dependencies,
-                        token,
-                    )
-    else:
+    if len(dependency_tree.children(root)) == 0:
         tag = dependency_tree.nodes[root].data.type
         index = dependency_tree.nodes[root].identifier
         if common.relationship.get(tag, -10) == 0 and not commonApi.should_retain(
             index
         ):
             sentence = cut_subtree_from_dependency_tree(
-                dependency_tree, index, tokens_remain, dependencies, token
+                dependency_tree, index, remaining_tokens_indices, dependencies, tokens
             )
-            return sentence, tokens_remain
-        else:
-            pass
-    return sentence, tokens_remain
+        return sentence, remaining_tokens_indices
+
+    # len(dependency_tree.children(root)) > 0
+    nodes = dependency_tree.children(root)
+    # Nodes with the number of child nodes equal to 0 are arranged from left to right in the order in the relationship tree.
+    nodes1 = [x for x in nodes if len(dependency_tree.children(x.identifier)) == 0]
+    # Nodes with the number of child nodes greater than 0 are arranged from large to small according to the number of child nodes.
+    nodes2 = [x for x in nodes if len(dependency_tree.children(x.identifier)) != 0]
+    nodes2.sort(key=lambda x: len(dependency_tree.children(x.identifier)))
+    nodes2.reverse()
+
+    nodes = nodes2 + nodes1
+
+    # Start recursively traversing the dependency tree.
+    for node in nodes:
+        index = node.identifier
+        tag = node.data.type
+        if (
+            # Relationship 1 refers to either of the following:
+            # dependent (dep)
+            # auxiliary verbs or copula (aux, auxpass, cop),
+            # clausal complement and open clausal complement (ccomp, xcomp),
+            # expletive (expl) - e.g. "there are", "here is" and "it is", "indeed", “in fact”
+            # determiner (det)
+            # multi-word expression (mwe)
+            # marker (mark)
+            # particle (prt)
+            # goes with (goeswith)
+            # case marker (case)
+            common.relationship.get(tag, -10) == 1
+            # No relationship.
+            or common.relationship.get(tag, -10) == -10
+            # Relationship 3 refers to either of the following:
+            # Object-related dependencies (obj, dobj, iobj)
+            # Subject-related dependencies (nsubj, csubj, xsubj)
+            or common.relationship.get(tag, -10) == 3
+        ):
+            if len(dependency_tree.children(index)) > 0:
+                sentence, remaining_tokens_indices = pruning(
+                    dependency_tree,
+                    index,
+                    dependencies,
+                    tokens,
+                    remaining_tokens_indices,
+                    sentence,
+                )
+        # punctuation (punct)
+        # parataxis
+        elif common.relationship.get(tag, -10) == 4:
+            return sentence, remaining_tokens_indices
+        # adjectival clause modifier (acl)
+        # adverbial modifier (advmod)
+        # compound (compound)
+        elif common.relationship.get(tag, -10) == 0:
+            if len(dependency_tree.children(index)) > 0:
+                sentence, remaining_tokens_indices = pruning(
+                    dependency_tree,
+                    index,
+                    dependencies,
+                    tokens,
+                    remaining_tokens_indices,
+                    sentence,
+                )
+                # After pruning the subtree
+                if len(dependency_tree.children(index)) >= 0:
+                    cut_subtree_from_dependency_tree(
+                        dependency_tree,
+                        index,
+                        remaining_tokens_indices,
+                        dependencies,
+                        tokens,
+                    )
+            # If there is no need to retain
+            elif not commonApi.should_retain(index):
+                cut_subtree_from_dependency_tree(
+                    dependency_tree,
+                    index,
+                    remaining_tokens_indices,
+                    dependencies,
+                    tokens,
+                )
+
+    return sentence, remaining_tokens_indices
 
 
 def cut_subtree_from_dependency_tree(
